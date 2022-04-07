45cf882e89de548c32bbafb0a2ca875a4326ddd5
TanujKS <tanujsiripurapu@gmail.com>
2022-04-07 12:11:42 -0700

    Unstable

--- backend/api/server.py
+++ backend/api/server.py
@@ -7,12 +7,13 @@ sys.path.append(os.getcwd())
 os.chdir(sys.path[0])
 
 import utils.utils as utils
+import utils.exceptions as exceptions
 from flask import Flask, json, jsonify
 from flask_restful import Resource, Api, reqparse
 from trader import Trader
 
-path = f"{sys.path[len(sys.path)-1]}/backend/api/user_data.csv"
-example_data = utils.csv_to_list(path)
+def update_data(path=f"{sys.path[len(sys.path)-1]}/api/user_data.csv"):
+    return utils.csv_to_list(path)
 
 def get_user(name):
     for user in example_data:
@@ -22,24 +23,40 @@ def get_user(name):
 
 app = Flask(__name__)
 api = Api(app)
+example_data = update_data()
 
-
-class Users(Resource):
+class TraderAPI(Resource):
     def get(self, name):
+        global example_data
+        example_data = update_data()
         return jsonify(get_user(name))
 
-    def post(self, name, coin, amount):
+    def post(self, operation, name, coin, amount):
         trader = Trader.from_dict(get_user(name))
-        trader.buy(coin, amount)
+        if operation == "buy":
+            func = trader.buy
+        elif operation == "sell":
+            func = trader.sell
+        else:
+            res = f"Invalid Operation: {operation}"
+
+        if func:
+            try:
+                res = func(coin, amount)
+            except exceptions.TradeError as error:
+                res = str(error)
+
         data = {
-        'name': name,
-        'coin': coin,
-        'amount': amount
+        'message': res,
         }
+
+        global example_data
+        example_data = update_data()
         return jsonify(data)
 
 
-api.add_resource(Users, '/users/<string:name>/', '/users/<string:name>/buy/<string:coin>/<int:amount>/')
+
+api.add_resource(TraderAPI, '/trader/<string:name>/', '/trader/<string:name>/<string:operation>/<string:coin>/<int:amount>/')
 
 
 if __name__ == '__main__':
