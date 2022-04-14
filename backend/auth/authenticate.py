b05118fdbf069d5233b6ed15351c2ef07e697d28
HarjyotS <sahh7239@lgsstudent.org>
2022-04-14 14:22:12 -0700

    further path updates, for compat

--- backend/auth/authenticate.py
+++ backend/auth/authenticate.py
@@ -1,18 +1,32 @@
+import os
+import sys
+from pathlib import Path
+
+if __name__ == "__main__":
+    os.chdir("..")
+    os.chdir("..")
+    sys.path.append(os.getcwd())
+    os.chdir(sys.path[0])
+
+
 from utils import exceptions
 
 import sqlite3
 from contextlib import closing
-import os
 
-path = f"{os.getcwd()}/maindb/tokens.db"
+path = f"{Path(__file__).parents[1]}/maindb/tokens.db"
 
 
 def authenticate(username, token):
     with closing(sqlite3.connect(path, isolation_level=None)) as con:
         with closing(con.cursor()) as cur:
-            tok = cur.execute("SELECT token FROM tokens WHERE id =?", (username.lower(),)).fetchone()
+            tok = cur.execute(
+                "SELECT token FROM tokens WHERE id =?", (username.lower(),)
+            ).fetchone()
             if not tok:
-                raise exceptions.AccountDoesNotExist(f"Account: '{username}' does not exist or has not been logged in")
+                raise exceptions.AccountDoesNotExist(
+                    f"Account: '{username}' does not exist or has not been logged in"
+                )
             elif token != tok[0]:
                 raise exceptions.InvalidToken("Improper token passed")
             else:
--- backend/auth/register.py
+++ backend/auth/register.py
@@ -1,22 +1,37 @@
 from utils import exceptions
+import os, sys
+from pathlib import Path
+
+if __name__ == "__main__":
+    os.chdir("..")
+    os.chdir("..")
+    sys.path.append(os.getcwd())
+    os.chdir(sys.path[0])
 
 import sqlite3
-import os
 from contextlib import closing
 
 
 def register(*, email, username, password):
-    path = f"{os.getcwd()}/maindb/data.db"
+    path = f"{Path(__file__).parents[1]}/maindb/data.db"
     with closing(sqlite3.connect(path, isolation_level=None)) as con:
         with closing(con.cursor()) as cur:
-            data = (cur.execute("SELECT * FROM users WHERE id =?", (username.lower(),))).fetchall()
+            data = (
+                cur.execute("SELECT * FROM users WHERE id =?", (username.lower(),))
+            ).fetchall()
 
             if data:
-                raise exceptions.AccountAlreadyExists(f"{username} is already an account")
+                raise exceptions.AccountAlreadyExists(
+                    f"{username} is already an account"
+                )
             else:
-                data = (cur.execute("SELECT * from users WHERE email = ?", (email.lower(), ))).fetchall()
+                data = (
+                    cur.execute("SELECT * from users WHERE email = ?", (email.lower(),))
+                ).fetchall()
                 if data:
-                    raise exceptions.AccountAlreadyExists(f"An account with {email} already exists")
+                    raise exceptions.AccountAlreadyExists(
+                        f"An account with {email} already exists"
+                    )
 
             cur.execute(
                 """INSERT INTO users (email, id, password)
